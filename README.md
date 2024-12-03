dac98c8ac538498d0c68e1c20774345600cc8301
Simon Kiefhaber <simon.kiefhaber@outlook.de>
2024-12-03 19:31:31 +0100

    day03

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 25 days, 25 languages
 
 
-| Day                  | Programming Language |
-|----------------------|----------------------|
-| 01                   | Excel                |
-| 02                   | Haskell              |
+| Day | Programming Language |
+|-----|----------------------|
+| 01  | Excel                |
+| 02  | Haskell              |
+| 03  | sed + grep           |
---
+++ src/day03.sh
@@ -0,0 +1,8 @@
+﻿#!/bin/sh
+# Calling: ./day03.sh <path_to_input>
+
+# Part 1:
+grep -Eo 'mul\(([0-9]{1,3}),([0-9]{1,3})\)' $1 | sed -nE 's/mul\(([0-9]{1,3}),([0-9]{1,3})\)/\1*\2/p' | paste -sd "+" - | bc
+
+# Part 2:
+paste -s inputs/day03 | sed -E "s/don't\(\)/\ndon't\(\)/g" | sed -E "s/do\(\)/\ndo\(\)/g" | grep -v "^don't\(\)" | grep -Eo 'mul\(([0-9]{1,3}),([0-9]{1,3})\)' | sed -nE 's/mul\(([0-9]{1,3}),([0-9]{1,3})\)/\1*\2/p' | paste -sd "+" - | bc
