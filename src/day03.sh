7e96ac8d61495731e75db88769f5b835495de613
Simon Kiefhaber <simon.kiefhaber@outlook.de>
2024-12-03 19:49:07 +0100

    day03

--- src/day03.sh
+++ src/day03.sh
@@ -5,4 +5,4 @@
 grep -Eo 'mul\(([0-9]{1,3}),([0-9]{1,3})\)' $1 | sed -nE 's/mul\(([0-9]{1,3}),([0-9]{1,3})\)/\1*\2/p' | paste -sd "+" - | bc
 
 # Part 2:
-paste -s inputs/day03 | sed -E "s/don't\(\)/\ndon't\(\)/g" | sed -E "s/do\(\)/\ndo\(\)/g" | grep -v "^don't\(\)" | grep -Eo 'mul\(([0-9]{1,3}),([0-9]{1,3})\)' | sed -nE 's/mul\(([0-9]{1,3}),([0-9]{1,3})\)/\1*\2/p' | paste -sd "+" - | bc
+paste -s $1 | sed -E "s/don't\(\)/\ndon't\(\)/g" | sed -E "s/do\(\)/\ndo\(\)/g" | grep -v "^don't\(\)" | grep -Eo 'mul\(([0-9]{1,3}),([0-9]{1,3})\)' | sed -nE 's/mul\(([0-9]{1,3}),([0-9]{1,3})\)/\1*\2/p' | paste -sd "+" - | bc
